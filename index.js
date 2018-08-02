a842615dd1a6260a5818d2980deaa6d0458ba671
Vitor Nogueira <vnweb.contato@gmail.com>
2018-08-02 17:47:11 -0300

    Add default broker options

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const { defaultsDeep } = require('lodash');
 const consumer = require('./src/consumer');
 const publisher = require('./src/publisher');
 
-module.exports = options => ({
-  consume: consumer(options),
-  publish: publisher(options),
-});
+module.exports = (brokerOptions) => {
+  const defaultOptions = { logger: console, disableLog: false };
+  const options = defaultsDeep({}, defaultOptions, brokerOptions);
+
+  return {
+    consume: consumer(options),
+    publish: publisher(options),
+  };
+};
